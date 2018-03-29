996b92047f8c2971e1f93d0262f14ed70f4310a7
Mateusz Sadowski <sadowskim@student.mini.pw.edu.pl>
2018-03-29 22:17:27 +0200

    Initial commit

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#function to display commands
+print() { echo "\$ $@" ; "$@" ; }
+
+workingdirpath=`echo $PWD`
+while true; do
+    read -p "Do you wish to download and install Buildroot?(y/n) " yn
+    case $yn in
+        [Yy]* )	print wget https://buildroot.org/downloads/buildroot-2018.02.tar.bz2;
+				print tar -xjf buildroot-2018.02.tar.bz2;
+				destdirpath=buildroot-2018.02;
+				print cd $destdirpath;
+				echo "Initalizing with configuration for RaspberryPi...";
+				print make raspberrypi_defconfig;
+				break;;
+        [Nn]* ) 
+				read -p "Provide path to Buildroot root directory: " destdirpath
+				print cd $destdirpath
+				break;;
+    esac
+done
+
+echo "Copying configuration files...";
+print cp $workingdirpath/.config $destdirpath;
+print cp -r $workingdirpath/mini $destdirpath/board;
+echo "Done copying configuration files.";
+# while true; do
+# 		#TODO: make it work
+#     read -p "Do you wish to see the configuration before building an image?(y/n) " yn
+#     case $yn in
+#         [Yy]* )	print make menuconfig
+# 								break;;
+#         [Nn]* ) break;;
+#     esac
+# done
+print make;
+echo "Done. Target image is located at ./buildroot-2018.02/output/images"
