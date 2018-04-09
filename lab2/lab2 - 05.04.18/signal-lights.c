7e37c2aebb4781d41f15a84b43e462cd3451d502
Mateusz Sadowski <mateuszsadowski@Mateuszs-MacBook-Pro-2.local>
2018-04-09 23:44:33 +0200

    Added deBounce

--- lab2/lab2 - 05.04.18/signal-lights.c
+++ lab2/lab2 - 05.04.18/signal-lights.c
@@ -9,7 +9,7 @@
 //                      perror(source),kill(0,SIGKILL),\
 //                      exit(EXIT_FAILURE))
 
-enum Direction { Left = 0, Right = 1};
+//enum Direction { Left = 0, Right = 1};
 
 #define BLUE_LED 17
 #define WHITE_LED 18
@@ -19,13 +19,15 @@ enum Direction { Left = 0, Right = 1};
 #define BUTTON_2 22
 #define BUTTON_3 10
 
-void turnSignal(Direction);
-void hazardLights();
+//void turnSignal(Direction);
+void turnSignal(int pin);
+void hazardLights(int pin);
+void deBounce(int pin);
 
 int main(int argc, char* argv[])
 {
 	//Setup pins with BCM-GPIO pin numbering
-    if(wiringPiSetupSys() == -1)
+    if(-1 == wiringPiSetupSys())
     {
 		//ERR("wiringPiSetup");
 		exit(1);
@@ -34,18 +36,23 @@ int main(int argc, char* argv[])
 	//TODO: Is it necessary to set up mode for all pins again??
 
 	//Register handlers for interrupts on buttons
-	wiringPiISR(BUTTON_1, INT_EDGE_BOTH, &turnSignal(Direction.Left));
-	wiringPiISR(BUTTON_2, INT_EDGE_BOTH, &turnSignal(Direction.Right));
-	wiringPiISR(BUTTON_3, INT_EDGE_BOTH, &turnSignal(Direction.hazardLights));
+	// wiringPiISR(BUTTON_1, INT_EDGE_BOTH, &turnSignal(Direction.Left));
+	// wiringPiISR(BUTTON_2, INT_EDGE_BOTH, &turnSignal(Direction.Right));
+	// wiringPiISR(BUTTON_3, INT_EDGE_BOTH, &turnSignal(Direction.hazardLights));
+	wiringPiISR(BUTTON_1, INT_EDGE_BOTH, &turnSignal(BUTTON_1));
+	wiringPiISR(BUTTON_2, INT_EDGE_BOTH, &turnSignal(BUTTON_2));
+	wiringPiISR(BUTTON_3, INT_EDGE_BOTH, &hazardLights(BUTTON_3));
 
     return 0;
 }
 
-void turnSignal(Direction direction)
+void turnSignal(int pin)
 {
+	deBounce(pin);
+
 	while(true)		//TODO: Make it be interruptable by button press
 	{
-		if(direction == Direction.Left)
+		if(pin == BUTTON_1)
 		{
 			digitalWrite(BLUE_LED, 1);
 			delay(500);
@@ -75,8 +82,10 @@ void turnSignal(Direction direction)
 	}
 }
 
-void hazardLights()
+void hazardLights(int pin)
 {
+	deBounce(pin);
+
 	while(true)		//TODO: Make it be interruptable by button press
 	{
 		digitalWrite(BLUE_LED, 1);
@@ -88,5 +97,17 @@ void hazardLights()
 		digitalWrite(WHITE_LED, 0);
 		digitalWrite(GREEN_LED, 0);
 		digitalWrite(RED_LED, 0);
+	}
+}
+
+void deBounce(int pin)
+{
+	int result;
+	while(0 != (result = waitForInterrupt(pin, 100)))		//TODO: Choose best time period
+	{
+		if(-1 == result)
+		{
+			exit(1);
+		}
 	}
 }
