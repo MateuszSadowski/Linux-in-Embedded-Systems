c49a2d9a38ea4538967f736b22b6883e11c4a41f
Mateusz Sadowski <mateusz.sadowski1@gmail.com>
2018-05-23 19:01:47 +0200

    Add read from configuration

--- [LINSW LAB 4] Mateusz Sadowski/timelapse.c
+++ [LINSW LAB 4] Mateusz Sadowski/timelapse.c
@@ -1,4 +1,4 @@
-#include <wiringPi.h>
+//#include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -18,12 +18,19 @@
 #define CAPTUREDELAY_MINVALUE 1
 #define CAPTUREDELAY_MAXVALUE 86400
 
-static volatile int isCapturing = 0;
-static volatile int captureDelay = 1;
+#define CONFIGURATION_FILE "config.txt"
 
-static sem_t sem;
+static volatile int isCapturing = 1;
+static volatile int captureDelay = 1;
+static volatile char* saveLocation;
+
+//static sem_t sem;
 
 void usage(char* programName);
+void setup();
+void toggleCapture();
+void capture();
+int scanAndUpdateConfiguration(FILE* fp);
 
 int main(int argc, char* argv[])
 {
@@ -33,27 +40,32 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 
-    if(1 == argc)
+    if(2 == argc)
     {
-        char* errStr;
-        if(0 == (captureDelay = (int)strtonum(argv[1], CAPTUREDELAY_MINVALUE, CAPTUREDELAY_MAXVALUE, *errStr)))
-        {
-            fprintf(stderr, errStr);
-            exit(EXIT_FAILURE);
-        }
+        // char* errStr;
+        // if(0 == (captureDelay = (int)strtonum(argv[1], CAPTUREDELAY_MINVALUE, CAPTUREDELAY_MAXVALUE, *errStr)))
+        // {
+        //     fprintf(stderr, "%s", errStr);
+        //     exit(EXIT_FAILURE);
+        // }
+		captureDelay = atoi(argv[1]);
     }
 
-	setup();
+	// FILE* config;
+	// if(NULL == (config = fopen (CONFIGURATION_FILE , "r+")))
+	// {
+	// 	fprintf(stderr, "Error opening file.\n");
+	// 	exit(EXIT_FAILURE);
+    // }
+	// scanAndUpdateConfiguration(config);
 
-	printf("Start program...\n");
-	while(1)
-	{	//Wait for interrupt
-	signalTurnLeft();
-	signalTurnRight();
-	signalHazard();
-	printf("Waiting for interrupt again...\n");
-	delay(MAIN_DELAY);
-	}
+	//setup();
+
+	// printf("Starting to capture every %d seconds...\n", captureDelay);
+	// while(1)
+	// {
+
+	// }
 	
     return EXIT_SUCCESS;
 }
@@ -63,3 +75,85 @@ void usage(char *programName)
     fprintf(stderr, "USAGE: %s t\n", programName);
 	fprintf(stderr, "t - number of seconds to elapse between each capture");
 }
+
+// void setup()
+// {
+// 	//Setup pins with BCM-GPIO pin numbering
+//     if(-1 == wiringPiSetupSys())
+//     {
+// 		perror("wiringPiSetupSys");
+// 		exit(EXIT_FAILURE);
+// 	}
+
+// 	//Register handlers for interrupts on buttons
+// 	if(-1 == wiringPiISR(BUTTON_1, INT_EDGE_BOTH, &toggleCapture))
+// 	{
+// 		perror("wiringPiISR");
+// 		exit(EXIT_FAILURE);
+// 	}
+	
+// 	//Reset RED LED on start
+// 	digitalWrite(RED_LED, 0);
+// }
+
+void toggleCapture()
+{
+	FILE* config;
+	if(NULL == (config = fopen (CONFIGURATION_FILE , "r+")))
+	{
+		fprintf(stderr, "Error opening file.\n");
+		exit(EXIT_FAILURE);
+    }
+
+    if(1 == scanAndUpdateConfiguration)
+	{
+		isCapturing = 0;
+		fputs("0", config);
+	}
+	else
+	{
+		isCapturing = 1;
+		fputs("1", config);
+	}
+
+	if(EOF == fclose(config))
+		fprintf(stderr, "Error closing file.\n");
+}
+
+int scanAndUpdateConfiguration(FILE* fp)
+{
+	char* errStr;
+	char buff[255];
+	int i;
+	fscanf(fp, "%s", buff);
+	// i = (int)strtonum(buff, 0, 1, *errStr);
+	// if(0 == i && errStr != NULL)
+	// {
+	// 	fprintf(stderr, "%s", errStr);
+	// 	exit(EXIT_FAILURE);
+	// }
+	i = atoi(buff);
+	printf("str: %s, i: %d\n", buff, i);
+	fscanf(fp, "%s", buff);
+	// if(0 == (captureDelay = (int)strtonum(buff, CAPTUREDELAY_MINVALUE, CAPTUREDELAY_MAXVALUE, *errStr)))
+	// {
+	// 	fprintf(stderr, "%s", errStr);
+	// 	exit(EXIT_FAILURE);
+	// }
+	captureDelay = atoi(buff);
+	printf("str: %s, i: %d\n", buff, captureDelay);
+	fscanf(fp, "%s", buff);
+	saveLocation = buff;
+	printf("str: %s, i: %s\n", buff, saveLocation);
+	return i;
+}
+
+// void capture()
+// {
+// 	while(isCapturing)
+// 	{
+// 		printf("Captured a photo.\n");
+// 		//code here
+// 		delay(captureDelay);
+// 	}
+// }
